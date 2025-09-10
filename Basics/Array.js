ef15a5fb31d1a10f71c09890ec06d9e4b2a16091
SAB-Sahil <sahil.borkar@indexnine.com>
2025-09-10 16:02:09 +0530

    new

--- Basics/Array.js
+++ Basics/Array.js
@@ -4,7 +4,41 @@
 // const point = new Array(40);
 // console.log(point.length);
 
-let age = prompt("Enter your age:");
-age = Number(age); // OR use +age shorthand
-console.log("You are " + age + " years old.");
+// let age = prompt("Enter your age:");
+// age = Number(age); // OR use +age shorthand
+// console.log("You are " + age + " years old.");
+
+// var cars = ["Volvo", "BMW"];     // Allowed
+const cars = ["Volvo", "BMW"];   // Not allowed
+console.log(cars);
+{
+//   var cars = ["Volvo", "BMW"];   // Allowed
+  const cars = ["Volvo", "BMW"]; // Not allowed
+  console.log(cars);
+}
+(function () {
+  let x = "Hello!!";  // I will invoke myself
+  console.log(x);
+})();
+// console.log(cars);
+
+const arr = [1, 2, 3, 4, 5];
+const productOfAllNums = arr.reduce((total, currentItem) => {
+    console.log(`Total : ${total} , current ${currentItem}`);
+    
+//   return total * currentItem;
+}, 1);
+console.log(productOfAllNums); // Outputs 120;
+console.log(arr); // Outputs [1, 2, 3, 4, 5]
+
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+function processUserInput(callback) {
+  const name = "Sahil";
+  callback(name); // calls greet("Sahil")
+}
+
+processUserInput(greet);
 
--- Basics/loops.js
+++ Basics/loops.js
@@ -1,52 +1,57 @@
-// let i = 0;
-// do {
-// console.log(i);
-// i++;
-// } while (i < 5);
+// // let i = 0;
+// // do {
+// // console.log(i);
+// // i++;
+// // } while (i < 5);
 
-// const nums = [10, 20, 30];
-// for (const n in nums) {
-// console.log(n);
+// // const nums = [10, 20, 30];
+// // for (const n in nums) {
+// // console.log(n);
+// // }
+// // for(let i=0; i<nums.length; i++){
+// //     console.log(nums[i], " ");
+// // }
+
+// // const nums = [1, 2, 3];
+// // console.log("Normal Array for Map : ", nums);
+// // let sq = nums.map((n) =>{return n * 2});
+// // console.log("Normal Array : ", nums);
+// // console.log("After Map ", sq);
+
+// // console.log(" ");
+
+// // const are = [1, 2, 3, 4, 5];
+// // console.log("Normal Array for Filter : ", are);
+// // let evens = are.filter(n => n%2 == 0)
+// // console.log("Normal Array after Filter : ", are);
+// // console.log("After Filter ", evens);
+
+// // console.log(" ");
+
+// // const arr = [1, 2, 3];
+// // nums.forEach((n, i) => (
+// // console.log(`Index ${i} has value ${n}`)
+// // ));
+
+// {
+//   let a = 8;
 // }
-// for(let i=0; i<nums.length; i++){
-//     console.log(nums[i], " ");
-// }
+// // console.log(a);
 
-// const nums = [1, 2, 3];
-// console.log("Normal Array for Map : ", nums);
-// let sq = nums.map((n) =>{return n * 2});
-// console.log("Normal Array : ", nums);
-// console.log("After Map ", sq);
+// let x = "16" + "Volvo";
+// console.log(x);
 
-// console.log(" ");
+// let text1 = "     Hello World!     ";
+// console.log(text1);
 
-// const are = [1, 2, 3, 4, 5];
-// console.log("Normal Array for Filter : ", are);
-// let evens = are.filter(n => n%2 == 0)
-// console.log("Normal Array after Filter : ", are);
-// console.log("After Filter ", evens);
-
-// console.log(" ");
-
-// const arr = [1, 2, 3];
-// nums.forEach((n, i) => (
-// console.log(`Index ${i} has value ${n}`)
-// ));
-
-{
-  let a = 8;
-}
-// console.log(a);
-
-let x = "16" + "Volvo";
-console.log(x);
-
-let text1 = "     Hello World!     ";
-console.log(text1);
-
-let text2 = text1.trim();
-console.log(text2);
-console.log(text1);
+// let text2 = text1.trim();
+// console.log(text2);
+// console.log(text1);
 
 
-console.log(Math.random());
+// console.log(Math.random());
+
+
+ const are = [1, 2, 3, 4, 5];
+ are.map((a) => console.log(a)
+ )
